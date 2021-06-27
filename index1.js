c4395ecb6713311a61f47b592c714eae159ac621
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-06-27 08:19:50 +0200

    all exercises

--- index1.js
+++ index1.js
@@ -19,14 +19,14 @@ console.log(result)
 Create a variable named x containing the number 12. 
 */
 let variable1 = 12
-console.log(variable1)
+console.log(variable1 - 12)
 
-function variable(){
+/*...function variable(){
   let x = 12
   return x
 }
 console.log(variable())
-//
+...*/
 /* EXERCISE 3
 Create a variable named name containing the string John Doe.
 */
@@ -36,7 +36,7 @@ console.log(name)
 
 /* EXERCISE 4
 Execute a SUBTRACTION between the number 12 and the variable x, which stores the value 12.
-Then, UNCOMMENT the commented out lines.
+
 */
 /*...
 let x = 12
@@ -65,34 +65,41 @@ console.log(result1, result2)
 
 /* EXERCISE 6
 Write the code to print the literal value of a number x: its value must be less than 10. (ex.: if x is 1 print "one", if 5 print "five" etc.).
-Then, UNCOMMENT the commented out lines.
-*/
 
-function ex6(x){
-  if(x < 10){
-    console.log("two")
-  }
- /* WRITE YOUR CODE HERE */
- let numberAsString = x.toString()
- return numberAsString
+*/
+let x = 4
+if(x === 0){
+  console.log("zero")
+}else if (x === 1){
+  console.log("one")
+}else if(x === 2){
+  console.log("two")
+}else if(x === 3){
+  console.log("three")
+}else if(x === 4){
+  console.log("four")
+}else if(x === 5){
+  console.log("five")
+}else if(x === 6){
+  console.log("six")
+}else if(x === 7){
+  console.log("seven")
+}else if(x === 8){
+  console.log("eight")
+}else if(x === 9){
+  console.log("nine")
 }
 
-console.log(ex6(2)) // expected: "two"
+
 
 /* EXERCISE 7
 Return the largest between two numbers a and b using a ternary operator.
-Then, UNCOMMENT the commented out lines.
 */
 
-function ex7(a, b){
+function largest(a, b){
   const largest = a > b ? a : b
-
   return largest
 }
+console.log(largest(10, 20)) // expected: 20
 
-console.log(ex7(10, 20)) // expected: 20
 
-
-module.exports = {
-  ex1,ex2,ex3,ex4,ex5,ex6,ex7
-}
