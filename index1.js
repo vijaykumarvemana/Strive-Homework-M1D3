3ddcef188e0cb602b2d2ea406806c6112c2ee4c8
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-06-27 08:01:00 +0200

    updating five exercises

--- index1.js
+++ index1.js
@@ -12,56 +12,55 @@ ASSIGNMENT RULES
 Write the code to execute a SUM between the number 12 and 20.
 */
 
-function ex1(){
-   return 12+20
-}
+let result = 12 + 29
+console.log(result)
 
 /* EXERCISE 2
-Create a variable named x containing the number 12. Then, UNCOMMENT the "return x" line.
+Create a variable named x containing the number 12. 
 */
+let variable1 = 12
+console.log(variable1)
 
-function ex2(){
+function variable(){
   let x = 12
   return x
 }
+console.log(variable())
 //
 /* EXERCISE 3
-Create a variable named name containing the string John Doe Then, UNCOMMENT the "return name" line.
+Create a variable named name containing the string John Doe.
 */
 
-function ex3(){
-  let name = "John Doe"
-  return name
-}
+let name = "John Doe"
+console.log(name)
 
 /* EXERCISE 4
 Execute a SUBTRACTION between the number 12 and the variable x, which stores the value 12.
 Then, UNCOMMENT the commented out lines.
 */
-
-function ex4(){
+/*...
+let x = 12
+let subtraction = x - 12 
+console.log(subtraction)
+...*/
+function subtraction(){
   let x = 12
   let result = x - 12
   return result
 }
+console.log(subtraction())
 
 /* EXERCISE 5
 Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
 Verify that name1 is different from name2. 
 Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2).
 Eventually, return both result1 and result2
-Then, UNCOMMENT the commented out lines.
 */
-
-function ex5(){
-  let name1 = "john"
-  let name2 = "John"
-  let result1 = name1 === name2
-  let result2 = name1 === name2.toLowerCase()
-  return {
-    result1, result2
-  }
-}
+let name1 = "john"
+let name2 = "John"
+let result1 = name1 === name2
+let result2 = name1 === name2.toLowerCase()
+console.log(result1, result2)
 
 
 /* EXERCISE 6
